50da1f87c7266c21b74631fae16102047e2a8209
Gabrielhfalcao <99846326+Gabrielhfalcao@users.noreply.github.com>
2022-04-24 16:52:14 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,28 @@
+function logobrasil(){
+    var a = document.getElementById('pganhadores')
+    a.innerText = 'Veja nessa seção os principais concorrentes na briga pela taça do Brasileirão 2022. Clique em um dos times abaixo para ver a opinião do nosso redator.'
+    var b = document.getElementById('nometime')
+    b.innerText = 'Selecione um Time:'
+  }
+
+  function logoatletico(){
+    var a = document.getElementById('pganhadores')
+    a.innerText = 'Pelos altos investimentos realizados nos últimos anos e pelo título do brasileirão na temporada passada, fazem do Atlético Mineiro um forte candidato de Vencedor da Serie A de 2022.'
+    var b = document.getElementById('nometime')
+    b.innerText = 'Atlético Mineiro'
+  }
+
+  function logopalmeiras(){
+    var a = document.getElementById('pganhadores')
+    a.innerText = 'Pela vitória da Libertadores na temporada passada e por portar um grande elenco em seu plantel, o porco é um forte nome ao título do Brasileirão de 2022.'
+    var b = document.getElementById('nometime')
+    b.innerText = 'Palmeiras'
+  }
+
+  function logoflamengo(){
+    var a = document.getElementById('pganhadores')
+    a.innerText = 'Ganhador por duas edições seguidas do maior campeonato do Brasil nos anos de 2020 e 2021, e por ter nomes de peso no seu elenco, o Flamengo é sem dúvidas um nome de destaque na corrida da Série A.'
+    var b = document.getElementById('nometime')
+    b.innerText = 'Flamengo'
+  }
+  
